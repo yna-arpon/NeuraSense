3f56c8e0f25e4176069c117cffe4a17e82e623b4
Yna Arpon <ynaarpon4@gmail.com>
2025-02-04 12:38:33 -0700

    Patient form dynamic styling

--- src/pages/recording/recording.ts
+++ src/pages/recording/recording.ts
@@ -8,6 +8,27 @@ export class RecordingPage extends BasePage {
     initialize(): void {
         super.initialize();
         console.log("[RecordingPage] Setting up recording page functionality.");
-        // Setup recording page logic here
+        
+        
+        document.querySelectorAll("input[required]").forEach(input => {
+            // Dynamically add * on required fields
+            let label = document.querySelector(`label[for="${input.id}"]`);
+            if (label) {
+                label.innerHTML += ' <span style="color: #742B45;">*</span>';
+            }
+
+            // Check if input fields are valid 
+            const form = document.getElementById("patientFieldsContainer") as HTMLFormElement;
+            const submitBtn = document.getElementById("startRecordingBtn") as HTMLButtonElement;
+
+            function checkFormValidity() {
+                submitBtn.disabled = !form.checkValidity();
+            }   
+            
+            input.addEventListener("input", checkFormValidity);
+
+            // Initial check
+            checkFormValidity();
+        });
     }
 }
